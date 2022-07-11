2bad52d76b310c7e97c8f090139b254f94eeb8d1
menabaddo <edmundalexfresh@gmail.com>
2022-07-11 10:51:06 +0100

    SignUp_name has been completed, it needs to be tested properly, and more sign up questions will be added

---
+++ app/src/main/java/com/example/logit/signup_name.java
@@ -0,0 +1,15 @@
+package com.example.logit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signup_name extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_name);
+        getSupportActionBar().hide();
+    }
+}
